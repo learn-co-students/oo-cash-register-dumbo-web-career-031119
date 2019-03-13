9332b2a04037197c0b4a18ae1e27a42cd59aef45
DanFCo <danfcorreia@gmail.com>
2019-03-13 17:27:08 -0400

    ca-ching

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,60 @@
 
+require"pry"
+
+
+class CashRegister
+
+
+attr_accessor :discount, :total, :price
+
+
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items_array = []
+  end
+
+  def add_item(title,price,quantity = 1)
+    @total += price * quantity
+    @price = price
+    quantity.times{@items_array << title}
+
+  end
+
+  def apply_discount
+  if @discount == 0
+
+    "There is no discount to apply."
+
+  else
+      new_discount = @discount.to_f/100
+     @total = @total.to_f * (1.00 - new_discount)
+    "After the discount, the total comes to $800."
+  end
+end
+
+
+def items
+  @items_array
+end
+
+
+def void_last_transaction
+  @total -= @price
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
